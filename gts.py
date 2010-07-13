2b109d27cdf94de0bccd1e2178b3526d732955b3
trocaire <trocaire@0674696d-5eaf-bc29-c96d-b4ba51c5af4f>
2010-07-13 07:52:44 +0000

    Japanese nickname handling

--- gts.py
+++ gts.py
@@ -16,7 +16,7 @@ from subprocess import call
 from time import sleep
 import os
 
-print 'IR-GTS, rev. 21'
+print 'IR-GTS, rev. 22'
 s = system()
 if s == 'Darwin' or s == 'Linux':
     if os.getuid() != 0:
