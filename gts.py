42bb84c24be7aea4370a8acbb5e61a356603d887
trocaire <trocaire@0674696d-5eaf-bc29-c96d-b4ba51c5af4f>
2010-07-16 06:15:09 +0000

    more information in pokemon.txt

--- gts.py
+++ gts.py
@@ -16,7 +16,7 @@ from subprocess import call
 from time import sleep
 import os
 
-print 'IR-GTS, rev. 24'
+print 'IR-GTS, rev. 25'
 s = system()
 if s == 'Darwin' or s == 'Linux':
     if os.getuid() != 0:
